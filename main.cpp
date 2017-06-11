4b7fc50d39e9b9c4ec4c04dea8f75604617478bb
Pawel Ficek <ficek.pawel@gmail.com>
2017-06-11 18:16:23 +0200

    Text menu expanded

--- main.cpp
+++ main.cpp
@@ -37,15 +37,37 @@ int main()
 
         while (not game.hasEnded())
         {
-            int option;
+            int option = 0;
 
             std::cout << "Current player " << game.getCurrentPlayer().getId() << std::endl;
 
             std::cout << "Your cards:" << std::endl;
             printCards(game.getCurrentPlayer().getCards());
             std::cout << std::endl;
-            std::cout << "1. Select Cards" << std::endl;
+            std::cout << "1. Select cards" << std::endl;
+            std::cout << "2. Unselect all cards" << std::endl;
+            std::cout << "3. Throw selected cards" << std::endl;
+            std::cout << "4. Pass your turn" << std::endl;
             std::cin >> option;
+
+            while (option != 3 and option != 4)
+            {
+                switch (option)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        std::cout << "Passing turn" << std::endl;
+                        break;
+                    default:
+                        std::cout << "Wrong option" << std::endl;
+                }
+            }
+
             game.nextPlayer();
         }
     }
